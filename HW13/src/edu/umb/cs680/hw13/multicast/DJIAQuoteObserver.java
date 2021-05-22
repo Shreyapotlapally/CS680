c0ac49d919e924025ed2035f59da8dfbaa5ab0d5
Shreyapotlapally <78864377+Shreyapotlapally@users.noreply.github.com>
2021-05-22 00:03:57 -0400

    Delete HW13 directory

--- HW13/src/edu/umb/cs680/hw13/multicast/DJIAQuoteObserver.java
+++
@@ -1,5 +0,0 @@
-package edu.umb.cs680.hw13.multicast;
-
-public interface DJIAQuoteObserver {
-	public void updateDJIA(DJIAEvent o1);
-}
--- HW13/src/edu/umb/cs680/hw13/multicast/StockQuoteObserver.java
+++
@@ -1,6 +0,0 @@
-
-package edu.umb.cs680.hw13.multicast;
-
-public interface StockQuoteObserver {
-	public void updateStock(StockEvent s1);
-}
